e988f3f6a38491a87719bff5b082a1f19af514aa
Yeshas M P <yeshasprabhakar@gmail.com>
2020-10-10 23:03:04 +0530

    Added PDF function

--- js/script.js
+++ js/script.js
@@ -21,3 +21,29 @@ $(document).ready(function(){
       ($("#languagesForm").clone().insertBefore("#languageBtnRow"));
     });
 });
+
+function generatePDF() {
+  var opt = {
+    margin:       0.5,
+    pagesplit: true,
+    enableLinks : true,
+    isToggleStyle: false,
+    filename:     'myfile.pdf',
+    image:        { type: 'jpeg', quality: 1 },
+    html2canvas:  { scale: 2 },
+    jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+  };
+  
+  // Change background color before print
+  // document.body.style.backgroundColor = 'green';
+  $("#resume-body").css({"background":"#C7B9FF"});
+
+  // Choose the element that our invoice is rendered in.
+  const element = document.getElementById("resume-body");
+  // Choose the element and save the PDF for our user.
+  html2pdf()
+    .set(opt)
+    .from(element)
+    .save();
+    document.body.style.backgroundColor = '#C7B9FF';
+}
